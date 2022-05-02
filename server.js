77bff9436e8a7fc218da92eda447c4eea81d5593
toinoulemoche <avaurs28@gmail.com>
2022-05-02 15:44:24 +0200

    KO: Q2 affichage de la balise <h1>

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+const fs = require('fs');
+const path = require('path');
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-	console.log("HELLO WORLD ANTOINE");
+	const content = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf-8');
+	console.log(content);
 	res.end();
 });
 
