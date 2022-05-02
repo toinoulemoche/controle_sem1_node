2b102f4f71f6545b2f1e66b8f1cc161955b4d50a
toinoulemoche <avaurs28@gmail.com>
2022-05-02 16:01:31 +0200

    OK: Q3-Q4 implémentation des codes pour détecter les 404 et les POST

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-	if(req.url === "/"){
-		res.write('<h1> HELLO WORLD ANTOINE !</h1>');
+	try{
+		if(req.method != "GET"){
+			res.write('<h1> La méthode ". req.method . "n\'est pas autorisée!</h1>');
+			res.end();
+		}
+		if(req.url === "/"){
+			res.write('<h1> HELLO WORLD ANTOINE !</h1>');
+		}
+		else {
+     	 		res.writeHead(404, {'content-type':'text/html'});
+      			res.write('<h1>404 Page introuvable</h1>');
+      			res.end()
+    		}
+	} catch (err) {
+		res.writeHead(500, {'content-type':'text/html'});
+		res.write('<h1></h1>');
 	}
 	res.end();
 });
